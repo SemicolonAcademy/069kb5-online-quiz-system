e1fd7091dd75831b8f89a956d0b725899bfa0fa5
krajankandel <krajankandel@gmail.com>
2013-04-26 05:05:51 +0000

    new registration form added

---
+++ regfom.php
@@ -0,0 +1,67 @@
+<html >
+  <head>
+    
+    <title>New User </title>
+   
+
+  
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+      }
+
+      .form-signin {
+        max-width: 300px;
+        padding: 19px 29px 29px;
+        margin: 0 auto 20px;
+        background-color: #fff;
+        border: 1px solid #e5e5e5;
+        -webkit-border-radius: 5px;
+           -moz-border-radius: 5px;
+                border-radius: 5px;
+        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                box-shadow: 0 1px 2px rgba(0,0,0,.05);
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin input[type="text"],
+      .form-signin input[type="password"] {
+        font-size: 16px;
+        height: auto;
+        margin-bottom: 15px;
+        padding: 7px 9px;
+      }
+
+    </style>
+                                   
+  </head>
+
+  <body bgcolor:#8585856>
+
+    <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Sign Up</h2>
+<h3>It’s free and always will be.</h3>
+       		<input type="text" class="input-block-level" placeholder="First Name"><input type="text" class="input-block-level" placeholder="Last Name">
+			 <input type="text" class="input-block-level" placeholder="Your Email">
+			  <input type="text" class="input-block-level" placeholder="User Name">
+	        <input type="password" class="input-block-level" placeholder="Password">
+		<input type="password" class="input-block-level" placeholder=" Repeat Password">
+        </br><label class="radio">
+		
+          
+<input type="radio" value="male">male
+<input type="radio" value="female">female</br>
+        <button class="btn btn-large btn-primary" type="submit">Sign Up</button>  <button class="btn btn-large btn-primary" type="submit">Reset</button>
+      </form>
+
+    </div> 
+
+  </body>
+</html>
